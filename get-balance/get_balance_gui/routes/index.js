c3da2fc7a60f7c131b39dd7d45d5d9e71e2c1d54
laff <x12729231@student.ncirl.ie>
2020-08-20 09:50:37 -0700

    balanceupdateGUI

---
+++ get-balance/get_balance_gui/routes/index.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+
+
+router.get('/', function (req, res, next) {
+  res.redirect('/balance');
+});
+
+router.post('/', function (req, res, next) {
+  res.redirect('/balance');
+});
+
+router.get('/balance', function (req, res, next) {
+  res.render('index', { title: 'E-banking Balance Inquiry Service', error: null });
+});
+
+router.post('/balance', function (req, res, next) {
+  let { email, password, account, amount } = req.body
+  try {
+    var PROTO_PATH = __dirname + '/../../src/main/proto/balance.proto';
+
+    var packageDefinition = protoLoader.loadSync(
+      PROTO_PATH,
+      {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+      });
+
+    var balance_proto = grpc.loadPackageDefinition(packageDefinition).balance;
+
+    var client = new balance_proto.Inquirer('localhost:50071', grpc.credentials.createInsecure());
+
+    var call = client.getBalance({ email: email, password: password, account: account });
+    call.on('data', function (response) {
+      try {
+        res.render('index', { title: 'E-banking Balance Inquiry Service', error: response.message });
+      } catch (error) {
+        res.render('index', { title: 'E-banking Balance Inquiry Service', error: "Balance Inquiry Service is not available at the moment please try again later" });
+      }
+    });
+    call.on('end', function () {
+
+    });
+    call.on('error', function (e) {
+      res.render('index', { title: 'E-banking Balance Inquiry Service', error: "Balance Inquiry Service is not available at the moment please try again later" });
+    });
+    call.on('status', function (status) {
+      return
+    });
+
+  } catch (error) {
+    res.render('index', { title: 'E-banking Balance Inquiry Service', error: "Balance Inquiry Service is not available at the moment please try again later" });
+  }
+
+});
+
+module.exports = router;
